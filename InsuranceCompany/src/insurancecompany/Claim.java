66b846261ba1f8ebf23b8c1a0fab6befcfbaebc3
Andrey Kamyshanov <a.kamyshanov@tools4brokers.com>
2012-10-10 14:58:17 +0200

    Claim modifications

--- InsuranceCompany/src/insurancecompany/Claim.java
+++ InsuranceCompany/src/insurancecompany/Claim.java
@@ -16,6 +16,7 @@ public class Claim {
         Complex
     }
 
+    private int id;
     private int customerId;
     private String description;
     private Category category;
@@ -26,12 +27,20 @@ public class Claim {
         this.category = Category.Undefined;
     }
 
+    public int getId() {
+        return id;
+    }
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+    
     public int getCustomerId() {
         return customerId;
     }
 
-    public void setCategory(Category _category) {
-        category = _category;
+    public void setCategory(Category category) {
+        this.category = category;
     }
 
     public String getDescription() {
